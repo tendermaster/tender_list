3effdac9eab54d87f1caa91714c3cddf147e3b8d
aaa <taruntushar@outlook.com>
2023-03-28 20:54:09 +0530

    attachment signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from Pagy::OverflowError, with: :record_not_found
 
+
+  private
   def after_sign_in_path_for(users)
     queries_path
   end
-
-  private
 
   def record_not_found
     render 'home/page_404', status: 404
