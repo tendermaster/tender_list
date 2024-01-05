dc5323b52e3350490e22721b99451c4a01779089
tarun47m <taruntushar@outlook.com>
2024-01-06 02:41:12 +0530

    fix google auth, disable turbo js

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,11 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+# pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+# FIX: problem with google oauth
+# https://dev.to/rbazinet/hotwire-fix-for-cors-error-when-using-omniauth-3k36
+# cors
+#
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
