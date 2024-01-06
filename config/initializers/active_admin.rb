ed96e58fe0db17d6758a802b944887e96d7119f4
tarun47m <taruntushar@outlook.com>
2024-01-06 22:16:01 +0530

    active admin high load active_admin.rb

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -299,8 +299,10 @@ ActiveAdmin.setup do |config|
   # By default the index screen includes a "Filters" sidebar on the right
   # hand side with a filter for each attribute of the registered model.
   # You can enable or disable them for all resources here.
-  #
-  # config.filters = true
+
+  # https://github.com/activeadmin/activeadmin/issues/5752
+  # memory leak
+  config.filters = false
   #
   # By default the filters include associations in a select, which means
   # that every record will be loaded for each association (up
@@ -308,7 +310,7 @@ ActiveAdmin.setup do |config|
   # You can enabled or disable the inclusion
   # of those filters by default here.
   #
-  # config.include_default_association_filters = true
+  config.include_default_association_filters = false
 
   # config.maximum_association_filter_arity = 256 # default value of :unlimited will change to 256 in a future version
   # config.filter_columns_for_large_association = [
