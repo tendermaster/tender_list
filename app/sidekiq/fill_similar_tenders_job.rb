230b06fcbc338d99b58b38c812e5dd57a93599a6
tarun47m <taruntushar@outlook.com>
2024-01-31 22:56:30 +0530

    stop further same request until cache is filled

--- app/sidekiq/fill_similar_tenders_job.rb
+++ app/sidekiq/fill_similar_tenders_job.rb
@@ -2,6 +2,8 @@ class FillSimilarTendersJob
   include Sidekiq::Job
 
   def perform(query, exclude_id)
+    # to stop further same request until cache is filled
+    Rails.cache.write("similar_tenders/#{query}", [], expire_in: 10.minutes)
     tenders = Tender.where([
                              "ts_rank(tender_text_vector, websearch_to_tsquery('english', ?)) > 0.6 and id != ?",
                              query,
