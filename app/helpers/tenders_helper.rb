6a990d89cb41c58a3ff9ea966b8f245470f17e87
tarun47m <taruntushar@outlook.com>
2024-05-23 01:31:06 +0530

    user id not present while logged out

--- app/helpers/tenders_helper.rb
+++ app/helpers/tenders_helper.rb
@@ -2,6 +2,10 @@ module TendersHelper
   include Pagy::Frontend
 
   def tender_bookmarked?(user_id, tender_id)
+    if user_id.nil? || tender_id.nil?
+      return false
+    end
+
     marked = Bookmark.find_by({ user_id: user_id, tender_id: tender_id })
     if marked.present?
       true
