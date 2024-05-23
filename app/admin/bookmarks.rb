575752ac749ee38cb0608c107c694024ad8d1393
tarun47m <taruntushar@outlook.com>
2024-05-23 14:48:22 +0530

    added bookmark to admin

---
+++ app/admin/bookmarks.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Bookmark do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :user_id, :tender_id, :personal_note
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:user_id, :tender_id, :personal_note]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
