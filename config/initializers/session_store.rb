caa01698e73b3cc32e68bf4d1af1164c20523c4f
tarun47m <taruntushar@outlook.com>
2024-07-06 00:45:25 +0530

    increase client logout time

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,4 @@
-Rails.application.config.session_store :active_record_store, :key => '_sigmatenders_session'
+# https://stackoverflow.com/questions/5860950/setting-session-timeout-in-rails-3
+# https://api.rubyonrails.org/v7.1/classes/ActionDispatch/Session/CookieStore.html
+#
+Rails.application.config.session_store :active_record_store, :key => '_sigmatenders_session', expire_after: 90.days
