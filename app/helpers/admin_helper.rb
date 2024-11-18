5a3505c99aa6282c5aac97f1be44a17aa8455a9b
tarun47m <taruntushar@outlook.com>
2024-11-18 13:31:18 +0530

    added admin id in admin dashboard

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -3,7 +3,7 @@ module AdminHelper
   def user_list
     users = []
     User.where('sign_in_count > 0').each do |u|
-      users.append([u.email.to_s, u.id])
+      users.append(["[##{u.id}] #{u.email.to_s}", u.id])
     end
 
     users.sort_by { |u| u[0] }
