3261be0e3232db991d4e33b038ecaf3072a09197
tarun47m <taruntushar@outlook.com>
2025-12-03 13:27:37 +0530

    added redis session env

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,21 @@
-# https://stackoverflow.com/questions/5860950/setting-session-timeout-in-rails-3
-# https://api.rubyonrails.org/v7.1/classes/ActionDispatch/Session/CookieStore.html
-#
+if Rails.env.development?
+  redis_host = '127.0.0.1'
+  redis_password = ''
+end
+
+if Rails.env.production?
+  redis_host = ENV['REDIS_HOST']
+  redis_password = ENV['REDIS_PASSWORD']
+end
+
+redis_port = 6379
+redis_db_sessions = 0
+
 Rails.application.config.session_store :redis_session_store,
   key: '_sigmatenders_session',
   redis: {
     expire_after: 90.days, # Cookie expiration
     ttl: 90.days, # Redis expiration, defaults to expire_after
     key_prefix: 'sigmatenders:session:',
-    url: 'redis://localhost:6379/0',
+    url: "redis://:#{redis_password}@#{redis_host}:#{redis_port}/#{redis_db_sessions}",
   }
