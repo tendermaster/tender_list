3dcb48510a77e4c4c3f7ee92be568f133a3a4ae4
tarun47m <taruntushar@outlook.com>
2026-02-04 15:28:11 +0530

    added subscription management

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,14 +5,23 @@ ActiveAdmin.register User do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at, :name, :role, :current_plan
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at, :name, :role, :current_plan]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :email, :name, :role, :current_plan
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :name
+    column :role
+    column :current_plan
+    column :sign_in_count
+    column :created_at
+    actions
+  end
+
+  filter :email
+  filter :name
+  filter :role
+  filter :current_plan
   
 end
