250aace70de2ce123d866480a3f1a334018e2f3c
tarun47m <taruntushar@outlook.com>
2026-02-04 16:01:30 +0530

    restored

--- app/admin/subscriptions.rb
+++ app/admin/subscriptions.rb
@@ -5,37 +5,14 @@ ActiveAdmin.register Subscription do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :plan_name, :order_id, :price, :start_date, :end_date, :user_id, :coupon_code
-
-  index do
-    selectable_column
-    id_column
-    column :user
-    column :plan_name
-    column :start_date
-    column :end_date
-    column :price
-    column :created_at
-    actions
-  end
-
-  filter :user
-  filter :plan_name, as: :select, collection: ['FREE', 'PAID', 'PREMIUM']
-  filter :start_date
-  filter :end_date
-  filter :coupon_code
-
-  form do |f|
-    f.inputs do
-      f.input :user
-      f.input :plan_name, as: :select, collection: ['FREE', 'PAID', 'PREMIUM'], include_blank: false
-      f.input :price
-      f.input :start_date, as: :datepicker
-      f.input :end_date, as: :datepicker
-      f.input :coupon_code
-      f.input :order_id
-    end
-    f.actions
-  end
+  # permit_params :plan_name, :order_id, :price, :start_date, :end_date, :user_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:plan_name, :order_id, :price, :start_date, :end_date, :user_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
   
 end
